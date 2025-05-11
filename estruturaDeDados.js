7e41a049d554fcedf1f4d1ac9c37ec0137874f0b
luizao <luizsantiago280@gmail.com>
2025-05-11 16:51:31 -0300

    :sparkles: resolvendo exercicios!

--- estruturaDeDados.js
+++ estruturaDeDados.js
@@ -21,7 +21,7 @@ class Dados {
                 i--;
             }
         }
-        return "[" + this.list + "]";
+        return this.toString(this.list);
     }
     removeMultiplos(multiplo) {
         if (multiplo <= 0) {
@@ -35,10 +35,39 @@ class Dados {
                 }
             }
         }
-        return "[" + this.list + "]";
+        return this.toString(this.list);
+    }
+    media() {
+        let sum = 0;
+        for (let i = 0; i < this.list.length; i++) {
+            sum += this.list[i];
+        }
+        sum = sum / this.list.length;
+        for (let i = 0; i < this.list.length; i++) {
+            if (Math.ceil(sum) === this.list[i]) {
+                return this.list[i];
+            }
+        }
+        return Math.ceil(sum);
+    }
+    reduzir(reduz) {
+        if (this.list.length <= reduz) {
+            return "Parametro maior ou igual ao tamanho da lista";
+        }
+        else {
+            for (let i = 0; i < this.list.length; i++) {
+                if (i > reduz) {
+                    this.list.splice(i);
+                    i--;
+                }
+            }
+            return this.toString(this.list);
+        }
     }
 }
 const dados = new Dados(5, 100);
 console.log(dados.returnList());
 // console.log(dados.valorCorte(2));
-console.log(dados.removeMultiplos(5));
+// console.log(dados.removeMultiplos(5));,
+// console.log(dados.media());
+console.log(dados.reduzir(2));
